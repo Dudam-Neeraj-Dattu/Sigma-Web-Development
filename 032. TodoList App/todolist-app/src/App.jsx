02d641b5a0d3586c37e4b96c9b1d9f4955e54aca
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-04-24 21:49:29 -0500

    idea 4 just a feature to show completed onl

--- 032. TodoList App/todolist-app/src/App.jsx
+++ 032. TodoList App/todolist-app/src/App.jsx
@@ -4,8 +4,39 @@ import Footer from './components/Footer'
 import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import TaskContainer from './components/TaskContainer';
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+   const [tasks, setTasks] = useState([]);
+   const [task, setTask] = useState("");
+
+
+   const handleAddClick = () => { 
+      task.length <= 3 ? alert('Enter a valid task') : setTasks([...tasks, { id: uuidv4(), task: task, completed: false }]);        
+      setTask("");      
+   }
+
+   const handleInputChange = (e) => {     
+      setTask(e.target.value);
+   }
+
+   const handleCheckbox = (id) => () => {
+      const updatedTasks = tasks.map((task) => {
+         if (task.id === id) {
+            return { ...task, completed: !task.completed };
+         }
+         return task;
+      });
+      setTasks(updatedTasks);
+   }
+   
+   useEffect(() => {
+     console.log(tasks);
+   }, [tasks]);
+   
+
    return (
       <div className="flex flex-col h-dvh">         
          <Navbar />
@@ -18,8 +49,8 @@ function App() {
                   <h2>Add Task</h2>
                </div>
                <div className="addTaskInput flex gap-2 my-4">
-                  <input className="py-2 px-5 bg-white w-full rounded-xl" type="text" placeholder="Enter your task here..." />
-                  <button className="bg-green-800 cursor-pointer px-3 py-2 rounded-xl text-white font-bold">Add</button>
+                  <input value={task} onChange={handleInputChange} className="py-2 px-5 bg-white w-full rounded-xl" type="text" placeholder="Enter your task here..." />
+                  <button onClick={handleAddClick} className="bg-green-800 cursor-pointer px-3 py-2 rounded-xl text-white font-bold">Add</button>
                </div>
                <div className="showCompleted flex gap-2 items-center">
                   <input className="cursor-pointer h-5 w-5" type="checkbox" name="" id="" />
@@ -27,19 +58,19 @@ function App() {
                </div>
             </div>
             <div className="tasksContainer flex-grow border-2 border-purple-800 rounded-lg w-3/4 px-5">
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
-               <TaskContainer />
+               {tasks.length === 0 && <h1 className="text-center text-2xl font-bold">No tasks added yet</h1>}
+               {tasks.map((task) => (
+                  <div key={task.id} className="taskContainer flex justify-between items-center gap-5 my-5 p-5 bg-purple-300 font-medium rounded-lg hover:bg-purple-100 hover:shadow-lg hover: shadow-purple-300">
+                     <div className="task flex gap-2 items-center">
+                        <input onChange={handleCheckbox(task.id)} className="h-7 w-8 cursor-pointer" type="checkbox" name="" id="" checked={task.completed} />
+                        <h1 className={task.completed ? "line-through" : ""}>{task.task}</h1>
+                     </div>
+                     <div className="taskButtons flex gap-2">
+                        <button className="bg-purple-800 cursor-pointer px-3 py-2 rounded-xl"><FaEdit className="h-5 w-8 invert" /></button>
+                        <button className="bg-red-600 cursor-pointer px-3 py-2 rounded-xl"><MdDeleteForever className="h-5 w-8 invert" /></button>
+                     </div>
+                  </div>
+               ))}
             </div>
          </div>
          <Footer />         
--- 032. TodoList App/todolist-app/src/components/TaskContainer.jsx
+++ 032. TodoList App/todolist-app/src/components/TaskContainer.jsx
@@ -4,10 +4,10 @@ import { MdDeleteForever } from "react-icons/md";
 
 const TaskContainer = () => {
    return (
-      <div className="taskContainer flex justify-center items-center gap-5 my-5 p-5 bg-purple-300 font-medium cursor-pointer rounded-lg hover:bg-purple-100 hover:shadow-lg hover: shadow-purple-300">
+      <div className="taskContainer flex justify-between items-center gap-5 my-5 p-5 bg-purple-300 font-medium rounded-lg hover:bg-purple-100 hover:shadow-lg hover: shadow-purple-300">
          <div className="task flex gap-2 items-center">
             <input className="h-8 w-8 cursor-pointer" type="checkbox" name="" id="" />
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed voluptate, ipsam enim aliquam possimus beataewqe architecto magni nisi repellat ut facere ab.</h1>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit non vitae qui veritatis. Laboriosam numquam nemo aliquam, maiores atque praesentium provident iste neque.</h1>
          </div>
          <div className="taskButtons flex gap-2">
             <button className="bg-purple-800 cursor-pointer px-3 py-2 rounded-xl"><FaEdit className="h-5 w-8 invert" /></button>
