6dd242710a27b08d4ae75b7538931f6cf6a9c135
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-05-11 21:31:22 -0500

    created redux store and slice

---
+++ 033. Password Manager/password-manager/src/redux/passwordSlice.js
@@ -0,0 +1,68 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialState = {
+   passwords: [],      // Stores all password objects
+   editId: null,       // ID of password being edited
+   form: {             // Current values in form
+      site: "",
+      username: "",
+      password: ""
+   }
+};
+
+const passwordSlice = createSlice({
+   name: 'passwords',
+   initialState,
+   reducers: {
+
+      setForm: (state, action) => {
+         state.form = action.payload;
+      },
+
+      // clearForm: (state) => {
+      //    state.form = {
+      //       site: "",
+      //       username: "",
+      //       password: ""
+      //    };
+      //    state.editId = null;
+      // },
+
+      startEdit(state, action) {
+         // console.log("Edit action payload:", action.payload);
+         const pw = state.passwords.find(p => p.id === action.payload);
+         if (pw) {
+            state.form = { site: pw.site, username: pw.username, password: pw.password };
+            state.editId = pw.id;
+         }
+      },
+
+      savePassword: (state) => {
+         const { site, username, password } = state.form;
+         if (state.editId) {
+            const index = state.passwords.findIndex(password => password.id === state.editId);
+            if (index !== -1) {
+               state.passwords[index] = { id: state.editId, site, username, password };
+            }
+         } else {
+            const newPassword = { id: uuidv4(), site, username, password };
+            state.passwords.push(newPassword);
+            // console.log(state.passwords);
+         }
+         state.form = {
+            site: "",
+            username: "",
+            password: ""
+         };
+         state.editId = null;
+      },
+
+      deletePassword(state, action) {
+         state.passwords = state.passwords.filter(p => p.id !== action.payload);
+      }
+   },
+});
+
+export const { populatePasswords, setForm, startEdit, savePassword, deletePassword } = passwordSlice.actions;
+export default passwordSlice.reducer;
