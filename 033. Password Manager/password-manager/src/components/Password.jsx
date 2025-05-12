1f49ca1cecaab580e31284890bc7559d2a760f8d
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-05-11 21:30:37 -0500

    added the functionality using props and redux store

--- 033. Password Manager/password-manager/src/components/Password.jsx
+++ 033. Password Manager/password-manager/src/components/Password.jsx
@@ -1,50 +1,83 @@
 import React from 'react'
+import { useState } from 'react'
+import { useDispatch } from "react-redux";
+import { startEdit, deletePassword } from "../redux/passwordSlice"
+import { ToastContainer, toast } from 'react-toastify';
 
 const Password = (props) => {
+
+   const [showPassword, setShowPassword] = useState(false);
+   const dispatch = useDispatch();
+
+   const togglePasswordVisibility = () => {
+      setShowPassword(prev => !prev);
+   }
+
+   const handleEdit = () => {
+      // console.log(props.id);
+      dispatch(startEdit(props.id));
+   }
+
+   const handleDelete = () => {
+      if (window.confirm("Are you sure you want to delete this password?")) {
+         dispatch(deletePassword(props.id));
+      }
+   }
+
+   const handleCopy = async (text) => {
+      console.log(text);
+      try {
+         await navigator.clipboard.writeText(text);         
+         toast.success('Text copied to clipboard');
+      } catch (err) {
+         toast.error('Failed to copy text: ', err);
+      }
+   }
+
    return (
       <>
-         <div className="py-2 px-4 border-b-2  border-r-2 border-l-2 border-green-400 col-span-3 flex items-center">{props.website}
-            <lord-icon className="cursor-pointer"
+         <div className="py-2 px-4 border-b-2  border-r-2 border-l-2 border-green-400 col-span-3 flex items-center">{props.site}
+            <lord-icon onClick={() => handleCopy(props.site)} className="cursor-pointer"
                style={{ "width": "25px", "height": "25px", "paddingTop": "3px", "paddingLeft": "3px" }}
                src="https://cdn.lordicon.com/iykgtsbt.json"
                colors="primary:16c72e"
                trigger="hover" >
             </lord-icon> </div>
          <div className="py-2 px-4 border-b-2 border-r-2 border-green-400 flex items-center">{props.username}
-            <lord-icon className="cursor-pointer"
+            {props.username.length > 0 && <lord-icon onClick={() => handleCopy(props.username)} className="cursor-pointer"
                style={{ "width": "25px", "height": "25px", "paddingTop": "3px", "paddingLeft": "3px" }}
                src="https://cdn.lordicon.com/iykgtsbt.json"
                trigger="hover" >
-            </lord-icon>
+            </lord-icon>}
          </div>
-         <div className="py-2 px-4 border-b-2 border-r-2 border-green-400 flex items-center">{props.password}
-            <lord-icon className="cursor-pointer"
+         <div className="py-2 px-4 border-b-2 border-r-2 border-green-400 flex items-center">{showPassword ? props.password : ('*').repeat(props.password.length)}
+            <lord-icon onClick={() => handleCopy(props.password)} className="cursor-pointer"
                style={{ "width": "25px", "height": "25px", "paddingTop": "3px", "paddingLeft": "3px" }}
                src="https://cdn.lordicon.com/iykgtsbt.json"
                trigger="hover" >
             </lord-icon>
          </div>
          <div className="py-2 px-4 border-b-2 border-r-2 border-green-400 flex items-center justify-center gap-3">
-            <lord-icon className="cursor-pointer"
+            <lord-icon onClick={togglePasswordVisibility} className="cursor-pointer"
                src="https://cdn.lordicon.com/dicvhxpz.json"
                trigger="hover"
                stroke="bold"
                colors="primary:#242424,secondary:#16c72e"
                style={{ "width": "30px", "height": "30px" }}>
             </lord-icon>
-            <lord-icon className="cursor-pointer"
+            <lord-icon onClick={handleEdit} className="cursor-pointer"
                src="https://cdn.lordicon.com/exymduqj.json"
                trigger="hover"
                stroke="bold"
                colors="primary:#242424,secondary:#16c72e"
                style={{ "width": "30px", "height": "30px" }}></lord-icon>
-            <lord-icon className="cursor-pointer"
+            <lord-icon onClick={handleDelete} className="cursor-pointer"
                src="https://cdn.lordicon.com/hwjcdycb.json"
                trigger="hover"
                stroke="bold"
                colors="primary:#242424,secondary:#16c72e"
                style={{ "width": "30px", "height": "30px" }}>
-            </lord-icon>
+            </lord-icon>            
          </div>
       </>
    )
--- 033. Password Manager/password-manager/src/components/PasswordDisplay.jsx
+++ 033. Password Manager/password-manager/src/components/PasswordDisplay.jsx
@@ -1,33 +1,30 @@
 import React from 'react'
 import Password from './Password'
+import { useSelector } from "react-redux";
 
 const Passwords = () => {
+   const passwords = useSelector((state) => state.passwords.passwords);
 
    return (
       <div className="flex justify-center m-auto w-[80%] mt-7 mb-10 ">
 
-         <div className="grid grid-cols-6 w-3/4">
-            {/* Header */}
-            <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-2 border-r-2 border-green-400 rounded-tl-lg sticky top-15.5 z-10 col-span-3">Website URL</div>
-            <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-b-2  border-r-2 border-t-2 text-center border-green-400 sticky top-15.5 z-10">Username</div>
-            <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-b-2 border-r-2 border-t-2  text-center border-green-400 sticky top-15.5 z-10">Password</div>
-            <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-b-2 border-t-2 border-r-2 text-center border-green-400 rounded-tr-lg sticky top-15.5 z-10">Actions</div>
+         {passwords.length === 0 ? (
+            <div className="text-center text-red-500 text-xl font-bold">No passwords saved yet.</div>
+         ) : (
+            <div className="grid grid-cols-6 w-3/4">
+               {/* Header */}
+               <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-2 border-r-2 border-green-400 rounded-tl-lg sticky top-15.5 z-10 col-span-3">Website URL</div>
+               <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-b-2  border-r-2 border-t-2 text-center border-green-400 sticky top-15.5 z-10">Username</div>
+               <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-b-2 border-r-2 border-t-2  text-center border-green-400 sticky top-15.5 z-10">Password</div>
+               <div className="bg-green-200 text-green-900 py-2 px-4 font-bold border-b-2 border-t-2 border-r-2 text-center border-green-400 rounded-tr-lg sticky top-15.5 z-10">Actions</div>
 
-            {/* Rows */}
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-            <Password website="The Sliding Mr. Bones (Next Stop, Pottersville)" username = "Malcolm Lockyer" password="Malcolm Lockyer" />
-         </div>
+               {/* Rows */}
+               {passwords.map((pw) => (
+                  <Password key={pw.id} id={pw.id} site={pw.site} username={pw.username} password={pw.password} />
+               ))}
+            </div>
+         )
+         }
       </div>
    )
 }
