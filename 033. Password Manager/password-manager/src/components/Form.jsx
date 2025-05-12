9fa02a2be1d13e931eebe120ad4b2455037f9a15
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-05-12 18:05:19 -0500

    added responsivenss to form

--- 033. Password Manager/password-manager/src/components/Form.jsx
+++ 033. Password Manager/password-manager/src/components/Form.jsx
@@ -37,32 +37,35 @@ const Manager = () => {
    };
 
    return (
-      <div className="flex flex-col justify-center items-center mt-6 w-[80%] m-auto gap-3">
-         <div className="name text-2xl font-bold">
+      <div className="flex flex-col justify-center items-center mt-6 w-[80%] m-auto gap-3 max-md:w-[95%]">
+         <div className="name text-2xl font-bold max-[480px]:hidden">
             <span className='text-green-500'> &lt;</span>
             <span>NeePass</span>
             <span className='text-green-500'>/&gt;</span>
          </div>
-         <div className="additional-info text-lg text-gray-700">
+         <div className="additional-info text-lg text-gray-700 max-[480px]:hidden">
             <p>Manage your passwords securely.</p>
          </div>
-         <div className="form flex flex-col justify-center items-center gap-4 w-3/4">
-            <div className="sitename w-full">
+         <div className="additional-info text-lg font-bold text-gray-700 min-[480px]:hidden">
+            <p>Enter the details.</p>
+         </div>
+         <div className="form flex flex-col justify-center items-center gap-4 w-3/4 max-xl:w-full">
+            <div className="sitename w-full max-[480px]:w-[90%]">
                <input value={form.site} onChange={handleChange} className="px-3 py-1.5 border-2 border-green-400 w-full rounded-2xl focus:border-green-200 focus:outline-none" type="url" name="site" placeholder="Enter the website URL" required />
             </div>
-            <div className="username relative w-full flex gap-10">
+            <div className="username relative w-full flex gap-10 max-[480px]:flex-col max-[480px]:gap-4 max-[480px]:w-[90%]">
                <input value={form.username} onChange={handleChange} className="px-3 py-1.5 border-2 border-green-400 w-full rounded-2xl focus:border-green-200 focus:outline-none" type="text" name="username" placeholder="Enter your username" />
                <input value={form.password} onChange={handleChange} className="px-3 py-1.5 border-2 border-green-400 w-full rounded-2xl focus:border-green-200 focus:outline-none" type={showPassword ? "text" : "password"} name="password" placeholder="Enter your password" required />
                {
                   showPassword ? (
                      <FaRegEye
-                        className='absolute cursor-pointer right-3 top-2'
+                        className='absolute cursor-pointer right-3 top-2 max-[480px]:top-16'
                         size={25}
                         onClick={togglePasswordVisibility}
                      />
                   ) : (
                      <FaRegEyeSlash
-                        className='absolute cursor-pointer right-3 top-2'
+                        className='absolute cursor-pointer right-3 top-2 max-[480px]:top-16'
                         size={25}
                         onClick={togglePasswordVisibility}
                      />
@@ -82,7 +85,14 @@ const Manager = () => {
                </button>
                <ToastContainer autoClose={2000} />
             </div>
-            <h1 className='self-start text-2xl font-bold text-green-800'>Your Passwords</h1>
+
+            <div className='w-[90%] min-[480px]:hidden h-[1px] bg-green-600'>
+
+            </div>
+
+            <div className="min-[480px]:self-start">
+            <h1 className='self-start text-2xl font-bold text-green-800' >Your Passwords</h1>
+            </div>
          </div>
       </div>
    )
